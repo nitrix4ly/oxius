269f1837103555c90e343ebfe9978c0e5f35b5dc
Nitrix <vaxeradev@gmail.com>
2025-06-19 18:59:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oxius',
-    version='1.0.2',
+    version='1.0.3',
     author='Nitrix',
     author_email='nitrixexe@outlook.com',
     description='Async FastAPI-style wrapper',
